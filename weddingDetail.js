f0d2985c98de6f021b6406641a7612824d09a8c2
Gabriel Nobel <nobel98@bluewin.ch>
2019-01-17 21:16:29 +0100

    Auto stash before merge of "master" and "origin/master"

--- weddingDetail.js
+++ weddingDetail.js
@@ -1,7 +1,7 @@
 var i = 1;
 setInterval(function() {
     i = i >= 10 ? 1 : ++i;
-    console.log(i >= 10);
-    console.log(i)
-    document.getElementById("img").src="images/us/" + i + ".jpg";
-  }, 2000);
+    let s = document.getElementById("img").src;
+    console.log()
+    document.getElementById("img").src= window.location.pathname.includes("/en/") ? "../images/us/" + i + ".png" : "images/us/" + i + ".png";
+  }, 4000);
